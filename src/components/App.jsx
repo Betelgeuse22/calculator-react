4d59dac7251a0561e9e68fce3ddcb3f101d70a75
Betelgeuse22 <Don.vitaki@gmail.com>
2023-06-29 00:18:42 +0200

    Add input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Box, Text, Button } from '@chakra-ui/react';
+import { Box, Text, Button, Input, Flex } from '@chakra-ui/react';
 import { evaluate } from 'mathjs';
 
 function Numbers(props) {
@@ -54,6 +54,51 @@ function CountButton(props) {
   );
 }
 
+function InputCalc(props) {
+  const [result, setResult] = useState('');
+  const [counts, setCounts] = useState('');
+
+  function updateCounts(e) {
+    const expressions =
+      /^[-+]?[0-9]+(\.[0-9]+)?([+\-*/][-+]?[0-9]+(\.[0-9]+)?)*$/;
+    const lastNumber = e.target.value[e.target.value.length - 2];
+
+    if (
+      !expressions.test(lastNumber) &&
+      !expressions.test(e.nativeEvent.data) &&
+      e.nativeEvent.data != null
+    ) {
+      return;
+    }
+
+    if (expressions.test(e.nativeEvent.data))
+      setResult(evaluate(e.target.value));
+    setCounts(e.target.value);
+  }
+
+  return (
+    <Flex w="100%" justifyContent="center">
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        border="2px"
+        borderRadius="8px"
+        borderColor="gray.50"
+      >
+        <Input
+          border="transparent"
+          type="text"
+          value={counts}
+          onInput={e => {
+            updateCounts(e);
+          }}
+        ></Input>
+        <Text>{result}</Text>
+      </Flex>
+    </Flex>
+  );
+}
+
 export const App = () => {
   const [counts, setCounts] = useState('0');
   const [result, setResult] = useState(' ');
@@ -80,6 +125,7 @@ export const App = () => {
           alignItems="center"
           w="200px"
         >
+          <InputCalc />
           <Box
             display="flex"
             w="100%"
