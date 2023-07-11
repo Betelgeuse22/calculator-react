644948f02e0330c89f4855e7d2ef414d93fb7318
Betelgeuse22 <Don.vitaki@gmail.com>
2023-07-12 01:22:10 +0200

    BurgerMenu

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,28 @@
 import { Calculator } from './Calculator';
+import { Menu } from './Menu';
+
+import { useState } from 'react';
+
+import { Box } from '@chakra-ui/react';
 
 export const App = () => {
+  const [mode, setMode] = useState('Calculator');
+  let application;
+
+  switch (mode) {
+    case 'Calculator':
+      application = <Calculator />;
+      break;
+    // case 'Converter':
+    //   application = <Converter />;
+    //   break;
+    default:
+      application = <Calculator />;
+  }
   return (
-    <>
-      <Calculator />
-    </>
+    <Box h="90vh">
+      <Menu onClick={setMode} />
+      {application}
+    </Box>
   );
 };
---
+++ src/components/Menu.jsx
@@ -0,0 +1,53 @@
+import { Box, Button, List, SlideFade, useDisclosure } from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
+
+export function Menu(onClick) {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Box>
+      <HamburgerIcon
+        w="45px"
+        h="45px"
+        p="5px"
+        m="5px"
+        borderRadius="5px"
+        onClick={onToggle}
+      />
+      <SlideFade in={isOpen} offsetY="-20px" unmountOnExit>
+        <Box
+          position="absolute"
+          bg="gray.100"
+          p="10px"
+          m="4px"
+          borderRadius="8px"
+          w="60%"
+          zIndex="10"
+        >
+          <List
+            display="flex"
+            flexDirection="column"
+            gap="10px"
+            fontSize="20px"
+          >
+            <Button
+              onClick={() => {
+                onClick('Calculator');
+              }}
+            >
+              Calculator
+            </Button>
+            <Button
+              onClick={() => {
+                onClick('Converter');
+              }}
+            >
+              Converter
+            </Button>
+            <Button>Settings</Button>
+          </List>
+        </Box>
+      </SlideFade>
+    </Box>
+  );
+}
